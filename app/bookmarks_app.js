74d275786182b3d91d6d40e70e81e270f6c24ac3
Igor Polynets <polinom100@gmail.com>
2011-09-23 12:19:54 -0400

    create fnew folder

--- app/bookmarks_app.js
+++ app/bookmarks_app.js
@@ -65,11 +65,9 @@ etv.vid.bmark.Folders = Backbone.Collection.extend({
 
 	model: etv.vid.bmark.Folder,
 
-	url: '/api/v3.0/media/lists?ptype=1'
+	url: '/api/v3.0/media/lists?ptype=1',
 
 });
-
-
 
 
 
@@ -99,7 +97,7 @@ etv.vid.bmark.BaseView = Backbone.View.extend({
    addOne: function(model){
       var children_view = new this.childrens_view({model: model, parent_view:this})
       this.children_views.push(children_view)
-      $(this.children_container).append(children_view.render().el)
+      $(this.children_container).prepend(children_view.render().el)
     },
 
    addAll: function(){
@@ -131,13 +129,14 @@ etv.vid.bmark.EntryView = etv.vid.bmark.BaseView.extend({
         },
 
     initialize: function(){
+         this.el = $(this.el)
          etv.vid.bmark.Entries.bind('reset', this.addAll, this);
          this.model.bind('remove', this.remove, this)
      },
 
      removeItem: function(){
        this.model.destroy();
-       this.remove()
+       this.el.hide('slow', function(){this.remove})
      }
 });
 
@@ -146,6 +145,8 @@ etv.vid.bmark.EntryView = etv.vid.bmark.BaseView.extend({
 
 //Single folder view
 etv.vid.bmark.FolderView = etv.vid.bmark.BaseView.extend({
+
+  edit_mod: false,
 
   opend: false,
 
@@ -164,8 +165,36 @@ etv.vid.bmark.FolderView = etv.vid.bmark.BaseView.extend({
   events: {
       "click #.open": "folderToggle",
       "click #.folder_remove": "removeFolder",
-
+      "click .save_edit" : 'toggleEditMod'
     },
+
+
+   initialize: function(attributes,options){
+        etv.vid.bmark.BaseView.prototype.initialize.call(this, attributes, options);
+        this.children_container.css({height:'0px;'})
+    },
+
+
+   toggleEditMod: function(e){
+      e.preventDefault()
+      if (!this.edit_mod) { this.turn_edit_mod()  } 
+      else { this.turn_of_edit_mod() }
+   },
+
+   turn_edit_mod: function() {
+     console.log('edit_mod')
+     this.edit_mod = true;
+     var val = this.el.find('.tit').text()
+     this.el.find('.tit').html('<input class="input_edit" value="'+val+'" type="text" ></input>')
+   },
+
+   turn_of_edit_mod: function() {
+    this.edit_mod = false
+     var val = this.el.find('.input_edit').val()
+     this.el.find('.tit').html(val)
+   },
+
+
 
   folderToggle: function(){
     if (!this.opend) {
@@ -181,9 +210,11 @@ etv.vid.bmark.FolderView = etv.vid.bmark.BaseView.extend({
     this.el.find('.open').html('CLOSE')
     this.opend = true
     this.el.attr({'open':true})
+    this.children_container.slideDown(1000)
   },
 
   closeFolder: function(){
+    this.children_container.slideUp(1000)
     this.el.find('.open').html('OPEN')
     this.opend = false
     this.children_container.html('')
@@ -192,14 +223,33 @@ etv.vid.bmark.FolderView = etv.vid.bmark.BaseView.extend({
 
   removeFolder: function(){
     this.model.destroy();
-    this.remove();
+    this.el.hide('fast', function(){this.remove})
   },
 
-  createFolder: function(){
-    console.log('Creqte Folder')
-  }
-});
+  render: function(){
+    if (this.model.get('shared')) {
+         this.model.set({ privat: true, shared: false})
+     } else {
+       this.model.set({ privat: false, shared: true})
+     };
 
+    $(this.el).html(this.template(this.model.toJSON()))
+
+
+    self = this
+    this.el.find('input').change(function(){
+          var val = $(this).attr('value')
+          self.sharedChanged(val)
+        })
+    return this
+  },
+
+  sharedChanged: function(val){
+    this.model.set({'shared':val})
+    this.model.save()
+  }
+
+});
 
 
 // Bookmarks Main Viev 
@@ -209,6 +259,10 @@ etv.vid.bmark.BookmarksView = etv.vid.bmark.BaseView.extend({
 
   childrens_view: etv.vid.bmark.FolderView,
 
+  events: {
+    "click #createNew": "emptyForm",
+  },
+
   initialize: function(attributes,options){
         this.model = new etv.vid.bmark.AppModel
         etv.vid.bmark.BaseView.prototype.initialize.call(this, attributes, options);
@@ -217,6 +271,20 @@ etv.vid.bmark.BookmarksView = etv.vid.bmark.BaseView.extend({
   
   closeAllChildren: function(){
     _.map(this.children_views, (function(view){ if (view.opend) {view.closeFolder()};  } ) )
+  },
+
+
+  emptyForm: function(){
+
+   var model = new etv.vid.bmark.Entry({ 'title':'', 'description':'', 'duration':'0', 'items':'0'});
+
+   var child_view =  new this.childrens_view({model:model});
+
+   this.children.add(model,{at:0})
+
+   this.children_views.push(child_view);
+
+   child_view.turn_edit_mod();
   }
 
 });
